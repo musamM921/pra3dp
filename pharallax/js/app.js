be62291e41f326cf26b03a2d75534f2bfcdb1cd6
musamM921 <135432572+musamM921@users.noreply.github.com>
2023-07-26 22:29:25 +0400

    update15

--- pharallax/js/app.js
+++ pharallax/js/app.js
@@ -1,7 +1,7 @@
 document.addEventListener('mousemove', e => {
 	Object.assign(document.documentElement, {
 		style: `
-		--move-x: ${(e.clientX - window.innerWidth / 2) * -.01}deg;
+		--move-x: ${(e.clientX - window.innerWidth / 2) * -.005}deg;
 		--move-y: ${(e.clientY - window.innerHeight / 2) * -.01}deg;
 		`
 	})
